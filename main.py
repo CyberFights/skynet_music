7ec5b6e717f5c297a733b30b3b26388883b671d9
CyberFights <shadowjer011@gmail.com>
2026-04-17 17:34:59 -0400

    Change Uvicorn port from 9000 to 3000

--- main.py
+++ main.py
@@ -85,5 +85,5 @@ async def o(music: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app=app, host="0.0.0.0", port=9000)
+    uvicorn.run(app=app, host="0.0.0.0", port=3000)
 
